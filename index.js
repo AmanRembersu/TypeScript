32f1be64e149287d0a076f7b24290acbda82ef09
Aman Rembersu <amanrembersu13@gmail.com>
2025-07-17 12:38:23 +0530

    Day 2 Typescript

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 // Day 1
 // console.log("Hello guys");
-// Day 2 - Variables
-var uname = "Aman"; // this is called type annotation 
-var age = 2; // here we cant give string or boolean bceause we set it to number
-console.log(uname, age);
+
--- index.ts
+++ index.ts
@@ -3,10 +3,5 @@
 
 // Day 2 - Variables
 
-let uname:string ="Aman"; // this is called type annotation 
-let age:number=2; // here we cant give string or boolean bceause we set it to number
-
-console.log(uname,age);
-
-// in javascript we can use number , string , boolean , null , undefined , object
-// new things in  typescript we canuse any , unknown , never , enum , tuple 
+let uname:string = "Aman";
+var age :boolean = true;
