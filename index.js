28e4a181a3e6731a3ceb04b58ff5ca053b4d9ed7
Aman Rembersu <amanrembersu13@gmail.com>
2025-07-17 12:36:37 +0530

    setting up env in typescript

---
+++ index.js
@@ -0,0 +1,6 @@
+// Day 1
+// console.log("Hello guys");
+// Day 2 - Variables
+var uname = "Aman"; // this is called type annotation 
+var age = 2; // here we cant give string or boolean bceause we set it to number
+console.log(uname, age);
